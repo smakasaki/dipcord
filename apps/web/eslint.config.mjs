0b400ffea5e4f28e81bff452a48a30bc7e33f598
smakasaki <salamandr258@gmail.com>
2025-03-05 16:29:30 +0200

    implement initial web application structure

---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,30 @@
+import createConfig from "@dipcord/eslint-config/create-config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+
+export default createConfig({
+    react: true,
+}, {
+    ignores: ["dist"],
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+    },
+    plugins: {
+        "react-hooks": reactHooks,
+        "react-refresh": reactRefresh,
+    },
+    rules: {
+        "antfu/top-level-function": "off",
+        "unicorn/filename-case": ["error", {
+            case: "kebabCase",
+            ignore: ["README.md", "~__root.tsx"],
+        }],
+        ...reactHooks.configs.recommended.rules,
+        "react-refresh/only-export-components": [
+            "warn",
+            { allowConstantExport: true },
+        ],
+    },
+});
